ce76be4379b26687939f289aa5c29847a6a545f0
alin <1179810071@qq.com>
2017-12-13 23:08:55 +0800

    更新

--- java/com/zl/toolbarandsearchviewdemo/MainActivity.java
+++ java/com/zl/toolbarandsearchviewdemo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.zl.toolbarandsearchviewdemo;
 
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,13 +17,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("标题");
-        toolbar.setNavigationIcon(android.R.drawable.ic_menu_add);
+//        toolbar.setTitle("标题");
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_search);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,"ToolBar",Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        MenuItem menuItem = menu.findItem(R.id.action_searchView);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+
+                break;
+            case R.id.action_searchView:
+
+                break;
+            default:
+
+                break;
+        }
+        return true;
     }
 }
