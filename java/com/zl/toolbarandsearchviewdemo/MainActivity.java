f07d7b6cdf9486aece407aa4c9389b94efbaa762
alin <1179810071@qq.com>
2017-12-15 15:33:46 +0800

    更新

--- java/com/zl/toolbarandsearchviewdemo/MainActivity.java
+++ java/com/zl/toolbarandsearchviewdemo/MainActivity.java
@@ -1,19 +1,29 @@
 package com.zl.toolbarandsearchviewdemo;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private String[] mDatas = {"aa", "bb", "cc", "dd"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,32 +32,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        toolbar.setTitle("标题");
         setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(android.R.drawable.ic_menu_search);
+        toolbar.setNavigationIcon(R.drawable.search);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"ToolBar",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "ToolBar", Toast.LENGTH_SHORT).show();
             }
         });
 
+        listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDatas));
+        listView.setTextFilterEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         //在Menu里面取SearchView
         MenuItem menuItem = menu.findItem(R.id.action_searchView);
         SearchView searchView = (SearchView) menuItem.getActionView();
 
         //设置页面绘制完成时直接显示搜索框
-        searchView.setIconified(false);
+//        searchView.setIconified(false);
 
         //设置页面绘制完成时直接显示搜索框并且不能被隐藏
 //        searchView.setIconifiedByDefault(false);
 
         //自定义扩展效果
-        ImageView imageView = searchView.findViewById(R.id.search_go_btn);
-        imageView.setImageResource(android.R.drawable.stat_sys_upload);
+        ImageView imageView = searchView.findViewById(R.id.search_button);
+        imageView.setImageResource(R.drawable.search);
         searchView.setSubmitButtonEnabled(true);
 
         //改变输入框的提示
@@ -59,52 +72,60 @@ public class MainActivity extends AppCompatActivity {
         searchView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"OnClickListener",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "OnClickListener", Toast.LENGTH_SHORT).show();
             }
         });
         searchView.setOnCloseListener(new SearchView.OnCloseListener() {
             @Override
             public boolean onClose() {
-                Toast.makeText(MainActivity.this,"OnCloseListener",Toast.LENGTH_SHORT).show();
+                if (listView.getVisibility() == View.VISIBLE) {
+                    listView.setVisibility(View.GONE);
+                }
                 return false;
             }
         });
         searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                Toast.makeText(MainActivity.this,"OnQueryTextFocusChangeListener",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "OnQueryTextFocusChangeListener", Toast.LENGTH_SHORT).show();
             }
         });
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                Toast.makeText(MainActivity.this,"QueryTextSubmit",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "QueryTextSubmit", Toast.LENGTH_SHORT).show();
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                Toast.makeText(MainActivity.this,"QueryTextChange",Toast.LENGTH_SHORT).show();
+                if (!TextUtils.isEmpty(newText)) {
+                    listView.setFilterText(newText);
+                } else {
+                    listView.clearTextFilter();
+                }
                 return false;
             }
         });
         searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
             @Override
             public boolean onSuggestionSelect(int position) {
-                Toast.makeText(MainActivity.this,"OnSuggestionListener",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "OnSuggestionListener", Toast.LENGTH_SHORT).show();
                 return false;
             }
 
             @Override
             public boolean onSuggestionClick(int position) {
-                Toast.makeText(MainActivity.this,"onSuggestionClick",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "onSuggestionClick", Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
         searchView.setOnSearchClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"OnSearchClickListener",Toast.LENGTH_SHORT).show();
+                if (listView.getVisibility() == View.GONE) {
+                    listView.setVisibility(View.VISIBLE);
+                }
             }
         });
         return true;
@@ -112,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_settings:
 
                 break;
